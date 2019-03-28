88de97a1a732c7d7ff4e3a0005982c79487fe0e0
Vlad Tarasevich <vlad.tarasevich27@gmail.com>
2019-03-28 02:37:54 -0700

    Set init

---
+++ Set/Set/Set.swift
@@ -0,0 +1,14 @@
+//
+//  Set.swift
+//  Set
+//
+//  Created by Vlad Tarasevich on 28/03/2019.
+//  Copyright © 2019 Vlad Tarasevich. All rights reserved.
+//
+
+import Foundation
+
+
+class Set {
+    // TODO : model logic here
+}
