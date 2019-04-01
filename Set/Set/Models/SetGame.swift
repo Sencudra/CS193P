1e73463fa7a9e8a41d8ffeeac5c7c1285bb68032
Vlad Tarasevich <vlad.tarasevich27@gmail.com>
2019-04-01 04:02:13 -0700

    Resolves #19, Resolves #20, Resolves #21, Resolves #22, Resolves #23, Resolves #24, Resolves #26, Resolves #27, Resolves #28, Resolves #31, Resolves #33, Resolves #34, Resolves #35

---
+++ Set/Set/Models/SetGame.swift
@@ -0,0 +1,164 @@
+//
+//  SetGame.swift
+//  Set
+//
+//  Created by Vlad Tarasevich on 28/03/2019.
+//  Copyright © 2019 Vlad Tarasevich. All rights reserved.
+//
+
+import Foundation
+
+
+// TODO write contanins that retruns Int?
+
+class SetGame {
+    
+    // MARK: - Semiprivate properties
+    
+    private(set) var setsFound: Int = 0
+    private(set) var table = [Card]()
+    private(set) var selected = [Card]()
+    
+    // MARK: - Private properties
+    private var deck = [Card]()
+    private var deckSize: Int {
+        return deck.count
+    }
+    
+    // MARK: - Public methods
+    
+    init() {
+        initializeDeck()
+        setsFound = 0
+        deal(numberOf: gameRules.cardsDealingOnStart)
+    }
+    
+    public func dealMoreCards(){
+        deal(numberOf: gameRules.cardsDealingDuringGame)
+    }
+    
+    public func touch(card: Card) -> Bool? {
+        
+        assert(table.contains(card), "SetGame.touchCard() : Card not found at the table!")
+        
+        if selected.count == gameRules.cardsToSelect {
+            
+            // Delete cards from table if it's a match
+            if checkSet(selected[0], selected[1], selected[2]) {
+                for card in selected {
+                    for index in 0..<table.count {
+                        if card == table[index] {
+                            table.remove(at: index)
+                            break
+                        }
+                    }
+                }
+                
+                // add new cards
+                dealMoreCards()
+                
+            }
+            
+            // remove selection anyway
+            selected.removeAll()
+            
+        }
+        
+        select(card: card)
+        
+        // Check three selected cards
+        if selected.count == gameRules.cardsToSelect {
+            if checkSet(selected[0], selected[1], selected[2]) {
+                setsFound += 1
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return nil
+        }
+        
+    }
+    
+    // MARK: - Private methods
+    
+    private func select(card: Card) {
+        
+        // Unselect card
+        for index in 0..<selected.count {
+            
+            if selected[index] == card {
+                print("remove")
+                selected.remove(at: index)
+                return
+            }
+        }
+        
+        // Select card
+        if (0..<gameRules.cardsToSelect).contains(selected.count) {
+            selected += [card]
+        } else {
+            assertionFailure("SetGame.selectCard(at \(index)) : Number of selected cards exceeded possible value")
+        }
+        
+    }
+    
+    private func initializeDeck() {
+        let range = 0...2
+        
+        for number in range {
+            for color in range {
+                for symbol in range {
+                    for filling in range {
+                        
+                        let card = Card(numberOfSymbols: number, symbol: symbol, color: color, filling: filling)
+                        deck += [card]
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        deck.shuffle()
+        
+        assert({12...}().contains(deckSize) && deckSize % 3 == 0, "Invalid number of cards in a deck! Must be in 12... and divisible by 3")
+    }
+    
+    private func deal(numberOf: Int) {
+        
+        if deck.count >= numberOf  {
+            for _ in 0..<numberOf {
+                let card = deck.removeFirst()
+                table += [card]
+            }
+            
+        }
+        
+    }
+    
+    private func checkSet(_ first: Card,_ second: Card,_ third: Card) -> Bool {
+        
+        let ruleNumber = Set([first.numberOfSymbols, second.numberOfSymbols, third.numberOfSymbols]).count
+        let ruleSymbol = Set([first.symbol, second.symbol, third.symbol]).count
+        let ruleColor = Set([first.color, second.color, third.color]).count
+        let ruleFilling = Set([first.filling, second.filling, third.filling]).count
+        
+        let checkRule = { $0 == 1 || $0 == gameRules.cardsToSelect }
+        
+        return checkRule(ruleNumber) && checkRule(ruleSymbol) && checkRule(ruleColor) && checkRule(ruleFilling)
+    }
+   
+}
+
+extension Dictionary where Value: Equatable {
+    
+    func key(for value: Value) -> Key? {
+        return first { $0.1 == value }?.0
+    }
+    
+}
+
+
---
+++ Set/Set/View/ViewController.swift
@@ -0,0 +1,166 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Vlad Tarasevich on 28/03/2019.
+//  Copyright © 2019 Vlad Tarasevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Private types
+    
+    private enum staticTexts {
+        
+        static var scoreLabel: String {
+            return "Sets found: "
+        }
+        
+    }
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak private var setsFoundLabel: UILabel!
+    @IBOutlet private var cardButtons: [UIButton]!
+    
+    // MARK: - Private properties
+    
+    private var game = SetGame()
+    
+    private var buttonToCard = [UIButton:Card]()
+    private lazy var buttonsEmpty: [UIButton] = cardButtons.shuffled()
+    
+    private var color = UIColor.blue.cgColor
+    
+    // MARK: - Private methods
+    
+    @IBAction private func touchCard(_ sender: UIButton) {
+        if let card = buttonToCard[sender] {
+            let match = game.touch(card: card)
+            color = getSelectionColor(if: match)
+            updateMapping()
+            
+            // TODO remove this crap
+            print("Card: number - \(card.numberOfSymbols+1), color - \(card.color), symbol - \(card.symbol), filling - \(card.filling)")
+            
+        } else {
+            print("ViewController.touchCard() - Card not found in cardButtons!")
+        }
+        
+    }
+    
+    @IBAction private func touchDealMoreCards(_ sender: Any) {
+        if buttonsEmpty.count > 0 {
+            game.dealMoreCards()
+            updateMapping()
+        } else {
+            print("ViewController.touchDealMoreCards() - No space for more cards")
+        }
+    }
+    
+    @IBAction private func touchNewGameButton(_ sender: Any) {
+        game = SetGame()
+        initCards()
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initCards()
+    }
+    
+    private func initCards() {
+        
+        buttonToCard = [UIButton:Card]()
+        buttonsEmpty = cardButtons.shuffled()
+        
+        for card in game.table {
+            buttonToCard[buttonsEmpty.removeFirst()] = card
+        }
+        updateView()
+        
+    }
+    
+    private func updateMapping() {
+        
+        let cards = game.table
+        
+        for button in buttonToCard.keys {
+            if let card = buttonToCard[button], !game.table.contains(card) {
+                buttonsEmpty += [button]
+                buttonToCard.removeValue(forKey: button)
+            }
+            
+        }
+            
+        // add new one
+        for card in cards {
+            if buttonToCard.key(for: card) == nil, buttonsEmpty.count > 0 {
+                buttonToCard[buttonsEmpty.removeFirst()] = card
+            }
+            
+        }
+        
+        updateView()
+    }
+    
+    private func updateView() {
+        
+        updateSetsFoundLabel()
+        
+        for button in cardButtons {
+            
+            // Checking if button have card binding
+            if let card = buttonToCard[button] {
+                
+                // Maintain card selection
+                if game.selected.contains(card) {
+                    button.layer.borderWidth = 3.0
+                } else {
+                    button.layer.borderWidth = 0.0
+                }
+                
+                button.layer.cornerRadius = 4.0
+                button.layer.borderColor = color
+                
+                // Getting nsatributed string
+                if let attributedString = CardPresenter.getContent(for: card) {
+                    button.setAttributedTitle(attributedString, for: .normal)
+                    button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+                    
+                } else {
+                    assertionFailure("ViewController.updateView() - There is no card here!")
+                    return
+                }
+            } else {
+                button.setTitle("", for: .normal)
+                button.setAttributedTitle(NSAttributedString(string: ""), for: .normal)
+                button.backgroundColor = #colorLiteral(red: 0.3333333333, green: 0.6666666667, blue: 0.3333333333, alpha: 1)
+                button.layer.borderWidth = 0.0
+            }
+        
+        }
+    
+    }
+    
+    private func updateSetsFoundLabel() {
+        let count = game.setsFound
+        setsFoundLabel.text = "\(staticTexts.scoreLabel)\(count)"
+    }
+    
+    private func getSelectionColor(if match: Bool?) -> CGColor {
+        switch match {
+        case true:
+            return UIColor.green.cgColor
+        case false:
+            return UIColor.red.cgColor
+        default:
+            return UIColor.blue.cgColor
+        }
+        
+    }
+
+}
